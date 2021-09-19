7f5a69353a329de9bcc52b11e1c4df35b3819fcc
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2021-09-19 12:03:12 -0300

    listando veiculos

---
+++ js/pages/vehicle/listVehicles.js
@@ -0,0 +1,36 @@
+function listVehicles() {
+  fetch('https://carrinho-virtual-iw-default-rtdb.firebaseio.com/vehicles.json')
+    .then(response => response.json())
+    .then(vehicles => {
+      const TABLE_VEHICLES = document.getElementById('table-list-vehicles');
+
+      for (let id in vehicles) {
+        TABLE_VEHICLES.innerHTML += `
+          <tr>
+            <td>${vehicles[id].make}</td>
+            <td>${vehicles[id].model}</td>
+            <td>
+              <button class="btn btn-warning btn-sm">Editar</button>
+              <button class="btn btn-danger btn-sm">Excluir</button>
+            </td>
+          </tr>
+        `
+      }
+    });
+
+  return `
+      <h2>Veiculos</h2>
+      <hr>
+
+      <table class="table table-hover table-striped">
+        <thead class="table-dark">
+          <tr>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody id="table-list-vehicles"></tbody>
+      </table>
+  `;
+}
