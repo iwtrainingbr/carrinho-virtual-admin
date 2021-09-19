98bdf739a69bafa4224c61447ba5021287b11076
Jacques <jacques_gm@hotmail.com>
2021-09-19 10:16:42 -0300

    Validando novos clientes

--- js/pages/client/addClient.js
+++ js/pages/client/addClient.js
@@ -3,25 +3,25 @@ function addClient() {
       <h2>Novo Cliente</h2>
       <hr>
 
-      <form action="">
+      <form action="#" onsubmit="validateAddClient(this)">
         <label for="name">Nome</label>
-        <input type="text" class="form-control w-50 p-3" id="name" placeholder="Nome do Cliente">
+        <input type="text" class="form-control w-50 p-3 " id="client-name" placeholder="Nome do Cliente">
         <br>
 
         <label for="email">Email</label>
-        <input type="email" class="form-control w-50 p-3" id="email" placeholder="Email">
+        <input type="email" class="form-control w-50 p-3" id="client-email" placeholder="Email">
         <br>
 
         <label for="phone">Celular</label>
-        <input type="tel" class="form-control w-50 p-3" id="phone" placeholder="Celular">
+        <input type="tel" class="form-control w-50 p-3" id="client-phone" placeholder="Celular">
         <br>
         
         <label for="address">Endereço</label>
-        <textarea class="form-control w-50 p-3" id="address" placeholder="Endereço"></textarea>
+        <textarea class="form-control w-50 p-3" id="client-address" placeholder="Endereço"></textarea>
         <br>
         
         <label for="data">Data de Nascimento</label>
-        <input type="date" class="form-control w-25 p-3" id="data">
+        <input type="date" class="form-control w-25 p-3" id="client-data">
         <br>
 
         <button class="btn btn-primary">Enviar</button>
---
+++ js/validate/client/validateAddClient.js
@@ -0,0 +1,33 @@
+function validateAddClient(event) {
+    event.preventDefault();
+
+    let valid = true;
+    const CLIENT_NAME = document.getElementById('client-name');
+    const CLIENT_EMAIL = document.getElementById('client-email');
+    const CLIENT_PHONE = document.getElementById('client-phone');
+    const CLIENT_ADDRESS= document.getElementById('client-address');
+    const CLIENT_DATE= document.getElementById('client-date');
+
+    if (CLIENT_NAME.value === '') {
+        CLIENT_NAME.classList.add('is-invalid');
+        valid = false;
+    }
+    if (CLIENT_EMAIL.value === '') {
+        CLIENT_EMAIL.classList.add('is-invalid');
+        valid = false;
+    }
+    if (CLIENT_PHONE.value === '') {
+        CLIENT_PHONE.classList.add('is-invalid');
+        valid = false;
+    }
+    if (CLIENT_ADDRESS.value === '') {
+        CLIENT_ADDRESS.classList.add('is-invalid');
+        valid = false;
+    }
+    if (CLIENT_DATE.value === '') {
+        CLIENT_DATE.classList.add('is-invalid');
+        valid = false;
+    }
+
+    return valid;
+}
