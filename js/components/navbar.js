235a940ab9f964fb98b892a3944f7ddddd7d6c1e
sahradria <falecomsahra@gmail.com>
2021-09-19 10:24:57 -0300

    agora temos o listrar produtos

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -5,6 +5,7 @@ const ROUTES = {
   "listClients": listClients(),
   "products": listProducts(),
   "addProduct": addProduct(),
+  "listOrders": listOrders(),
 };
 
 
@@ -17,6 +18,7 @@ function navbar() {
       <button data-component="menu" data-action="listClients" class="btn btn-outline-dark">Clientes</button>
       <button data-component="menu" data-action="products" class="btn btn-outline-dark">Produtos</button>
       <button data-component="menu" data-action="addProduct" class="btn btn-outline-dark">Novo Produto</button>
+      <button data-component="menu" data-action="listOrders" class="btn btn-outline-dark">Listar Pedidos</button>
     </nav>
   `;
 }
