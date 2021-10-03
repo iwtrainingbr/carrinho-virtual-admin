76e72648d918313439376044a162d4bb10e95e44
FelipeDeBoa <53651862+felipelisboadoc@users.noreply.github.com>
2021-10-03 10:07:50 -0300

    Add Produto Banco

--- js/validate/product/validateAddProduct.js
+++ js/validate/product/validateAddProduct.js
@@ -1,44 +1,62 @@
 
-
 function addNovoProduto() {
-    event.preventDefault();
-    let valid = true;
-    const INPUT_NOME = document.getElementById("product-name");
-    const INPUT_VALOR = document.getElementById("product-value");
-    const INPUT_FOTO = document.getElementById("product-photo");
-    const INPUT_DESCRICAO = document.getElementById("product-description");
-    const INPUT_CATEGORIA = document.getElementById("product-category");
-    const INPUT_QUANTIDADE = document.getElementById("product-quantity");
-      
-    if (INPUT_NOME.value === '') {
-        INPUT_NOME.classList.add('is-invalid');
-      valid = false;
-    }
-
-    if (INPUT_VALOR.value === '') {
-        INPUT_VALOR.classList.add('is-invalid');
-      valid = false;
-    }
-
-    if (INPUT_FOTO.value === '') {
-        INPUT_FOTO.classList.add('is-invalid');
-      valid = false;
-    }
-
-    if (INPUT_DESCRICAO.value === '') {
-        INPUT_DESCRICAO.classList.add('is-invalid');
-      valid = false;
-    }
-
-    if (INPUT_CATEGORIA.value === '') {
-        INPUT_CATEGORIA.classList.add('is-invalid');
-      valid = false;
-    }
-
-    if (INPUT_QUANTIDADE.value === '') {
-        INPUT_QUANTIDADE.classList.add('is-invalid');
-      valid = false;
-    }
-  
-    return valid;
+  event.preventDefault();
+  let valid = true;
+  const INPUT_NAME = document.getElementById("product-name");
+  const INPUT_PRICE = document.getElementById("product-value");
+  const INPUT_PHOTO = document.getElementById("product-photo");
+  const INPUT_DESCRIPTION = document.getElementById("product-description");
+  const INPUT_CATEGORY = document.getElementById("product-category");
+  const INPUT_QUANTITY = document.getElementById("product-quantity");
+    
+  if (INPUT_NAME.value === '') {
+      INPUT_NAME.classList.add('is-invalid');
+    valid = false;
   }
+
+  if (INPUT_PRICE.value === '') {
+      INPUT_PRICE.classList.add('is-invalid');
+    valid = false;
+  }
+
+  if (INPUT_PHOTO.value === '') {
+      INPUT_PHOTO.classList.add('is-invalid');
+    valid = false;
+  }
+
+  if (INPUT_DESCRIPTION.value === '') {
+      INPUT_DESCRIPTION.classList.add('is-invalid');
+    valid = false;
+  }
+
+  if (INPUT_CATEGORY.value === '') {
+      INPUT_CATEGORY.classList.add('is-invalid');
+    valid = false;
+  }
+
+  if (INPUT_QUANTITY.value === '') {
+      INPUT_QUANTITY.classList.add('is-invalid');
+    valid = false;
+  }
+
+  if (valid === false) {
+    return;
+  }
+
+  let newProduct = {
+    price: INPUT_PRICE.value,
+    photo: INPUT_PHOTO.value,
+    description: INPUT_DESCRIPTION.value,
+    category: INPUT_CATEGORY.value,
+    quantity: INPUT_QUANTITY.value,
+  };
+
+  fetch('https://carrinho-virtual-iw-default-rtdb.firebaseio.com/products.json', {
+    method: 'POST',
+    body: JSON.stringify(newProduct)
+  });
+
+  alert('Produto inserido com sucesso');
+
+  location.href = '/';
+}
