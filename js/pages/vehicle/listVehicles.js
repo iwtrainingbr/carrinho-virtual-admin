527f9231caac79160671243d1ab60e402e0bb382
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2021-10-03 11:30:24 -0300

    Excluindo veiculo do banco de dados

--- js/pages/vehicle/listVehicles.js
+++ js/pages/vehicle/listVehicles.js
@@ -1,8 +1,22 @@
-function listVehicles() {
+const removeVehicle = (id) => {
+  if (false === confirm('Você tem certeza?')) {
+    return;
+  }
+
+  fetch(API_URL + `/vehicles/${id}.json`, {
+    method: 'DELETE'
+  });
+
+  location.href = '/';
+}
+
+function findVehicles() {
   fetch(API_URL + '/vehicles.json')
     .then(response => response.json())
     .then(vehicles => {
       const TABLE_VEHICLES = document.getElementById('table-list-vehicles');
+
+      TABLE_VEHICLES.innerHTML = '';
 
       for (let id in vehicles) {
         TABLE_VEHICLES.innerHTML += `
@@ -11,12 +25,16 @@ function listVehicles() {
             <td>${vehicles[id].model}</td>
             <td>
               <button class="btn btn-warning btn-sm">Editar</button>
-              <button class="btn btn-danger btn-sm">Excluir</button>
+              <button onclick="removeVehicle('${id}')" class="btn btn-danger btn-sm">Excluir</button>
             </td>
           </tr>
         `
       }
     });
+}
+
+function listVehicles() {
+  findVehicles();
 
   return `
       <h2>Veiculos</h2>
